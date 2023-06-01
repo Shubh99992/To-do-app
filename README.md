2b1ae38ee6d6b69066b036530dd272d60ac396ee
shubh99992 <coderblade7@gmail.com>
2023-06-01 22:30:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flutter ToDo App
+
+A basic to-do app built with Flutter. This app has add/delete/update functinality with search for any existing to-do item in the app.
+
+## Screenshot
+
+![Flutter todo app](./flutter-todo-iphone.png)
---
+++ lib/model/todo.dart
@@ -0,0 +1,17 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      
+    ];
+  }
+}
